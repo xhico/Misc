1e68b0a74cbc6627730e842ce1f3fca21b732396
xhico <francgf@gmail.com>
2022-10-15 17:31:50 +0100

    Update Misc.py

--- Misc.py
+++ Misc.py
@@ -5,14 +5,6 @@ import base64
 import json
 
 
-def enc911():
-    with open("/home/pi/.911") as inFile:
-        data = inFile.read()
-
-    with open("/home/pi/.911", "wb") as outFile:
-        outFile.write(base64.b64encode(bytes(data, "utf-8")))
-
-
 def get911(key):
     with open("/home/pi/.911") as inFile:
         data = json.loads(base64.b64decode(inFile.read().encode("utf-8")).decode("utf-8"))
